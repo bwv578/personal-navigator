2229cc115f454ecc7f593f38ad93aa21ad2330e7
bwv578 <nanopico912@gmail.com>
2023-10-26 19:47:38 +0900

    프로젝트 생성

---
+++ src/test/java/com/navigator/application/NavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navigator.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
